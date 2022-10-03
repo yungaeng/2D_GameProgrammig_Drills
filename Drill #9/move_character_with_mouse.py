ba47c0ed2fa367229f2a9ebe17b8e7362513fd42
gunho Lee <ghqwe123@naver.com>
2022-10-03 15:01:58 +0900

    Drill #9 완성

--- Drill #9/move_character_with_mouse.py
+++ Drill #9/move_character_with_mouse.py
@@ -1,3 +1,4 @@
+import random
 from pico2d import *
 
 TUK_WIDTH, TUK_HEIGHT = 1280, 1024
@@ -23,19 +24,46 @@ open_canvas(TUK_WIDTH, TUK_HEIGHT)
 # fill here
 TUK_ground = load_image('TUK_GROUND.png')
 character = load_image('animation_sheet.png')
+arrow = load_image('hand_arrow.png')
 
 running = True
-x, y = TUK_WIDTH // 2, TUK_HEIGHT // 2
+sx, sy = TUK_WIDTH // 2, TUK_HEIGHT // 2
+x, y = sx, sy
+ax, ay = x, y
 frame = 0
 hide_cursor()
 
+t = 0
+def reset_world():
+    global ax,ay, t, sx, sy
+    ax, ay = random.randint(0,TUK_WIDTH), random.randint(0,TUK_HEIGHT)
+    t = 0
+    sx, sy = x, y
+    pass
+
+def update_world():
+    global x, y, sx, sy, ax, ay, t
+    t += 0.001
+    x = (1-t)*sx+t*ax
+    y = (1-t)*sy+t*ay
+
+    if t>=1.0:
+        reset_world()
+
+
+reset_world()
+
 while running:
+    update_world()
     clear_canvas()
     TUK_ground.draw(TUK_WIDTH // 2, TUK_HEIGHT // 2)
-    character.clip_draw(frame * 100, 100 * 1, 100, 100, x, y)
+    if((ax-x)>0):
+        character.clip_draw(frame * 100, 100 * 1, 100, 100, x, y)
+    elif((ax-x)<0):
+        character.clip_draw(frame * 100, 100 * 0, 100, 100, x, y)
+    arrow.draw(ax, ay)
     update_canvas()
     frame = (frame + 1) % 8
-
     handle_events()
 
 close_canvas()
